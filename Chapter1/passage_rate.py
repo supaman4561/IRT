761ff309058903343a628b328179962561565343
supaman4561 <dosu416@gmail.com>
2018-06-20 04:04:23 +0900

    feat: enable to confirm passage rate

--- Chapter1/passage_rate.py
+++ Chapter1/passage_rate.py
@@ -1,28 +1,34 @@
 import numpy as np
 
-data = []
+def calc_passage_rate(data):
+    passage_rate = []
+    for item in data.transpose():
+        passage_rate.extend(item.sum() / item.shape)
+    passage_rate = np.array(passage_rate)
+    return passage_rate
 
-filename = input("filename:")
-with open(filename, 'r') as f:
-    for row in f:
-        data.append([int(v) for v in list(row.strip('\n'))])
+if __name__ == '__main__':
 
-data = np.array(data)
+    data = []
 
-passage_rate = []
+    filename = input("filename:")
+    with open(filename, 'r') as f:
+        for row in f:
+            data.append([int(v) for v in list(row.strip('\n'))])
 
-for item in data.transpose():
-    passage_rate.extend(item.sum() / item.shape)
+    data = np.array(data)
 
-passage_rate = np.array(passage_rate)
+    passage_rate = calc_passage_rate(data)
 
-print(passage_rate)
+    print(passage_rate)
 
-dict = {}
+    dict = {}
 
-for i, p in enumerate(passage_rate):
-    dict.update({str(i+1):p})
+    for i, p in enumerate(passage_rate):
+        dict.update({str(i+1):p})
 
-# 通過率を基に項目を降順に表示
-for k, v in sorted(dict.items(), key=lambda x:-x[1]):
-    print(k, v)
+    # 通過率を基に項目を降順に表示
+    print("--- Ranking ---")
+    print("item passage_rate")
+    for k, v in sorted(dict.items(), key=lambda x:-x[1]):
+        print("{0:<5}{1}".format(k, v))
